759997427df16677b71ca4b4d596e46138f51728
zzlmilk <413124766@qq.com>
2012-12-13 15:32:21 +0800

    add controllers

---
+++ Summly/Controllers/SummlyListViewController/SummlyListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SummlyListViewController.h
+//  Summly
+//
+//  Created by zzlmilk on 12-12-13.
+//  Copyright (c) 2012年 zzlmilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummlyListViewController : UIViewController
+
+@end
---
+++ Summly/Controllers/SummlyListViewController/SummlyListViewController.m
@@ -0,0 +1,38 @@
+//
+//  SummlyListViewController.m
+//  Summly
+//
+//  Created by zzlmilk on 12-12-13.
+//  Copyright (c) 2012年 zzlmilk. All rights reserved.
+//
+
+#import "SummlyListViewController.h"
+
+@interface SummlyListViewController ()
+
+@end
+
+@implementation SummlyListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
